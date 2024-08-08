b1b06e14b364eaa7bff0f4a1f023aced10ff5d7a
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2024-08-07 21:13:34 -0600

    feat(Board): add handleClick function to manage Square state
        - Implemented handleClick function to control the state of each Square via Board.
        - Updated Board to handle state changes for Squares when clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,13 @@ const App = () => {
   )
 }
 
-const Square = () =>
+const Square = ({markType, onSquareClick}) =>
 {
   const [value,setValue] = useState(null)
 
-  const handleClick = () => setValue('X')
   return(
-    <button className='square' onClick={handleClick}>
-      {value}
+    <button className='square' onClick={onSquareClick}>
+      {markType}
     </button>
   )
 }
@@ -25,22 +24,31 @@ const Square = () =>
 // in JS "export" means this function can be used outside this file
 const Board = () =>
 {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    //This is where Board handles the state of every Square 
+    const nextSquares = squares.slice()
+    nextSquares[i] = "X";
+    setSquares(nextSquares)
+  }
+
   return(
     <>
     <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
+      <Square markType={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square markType={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square markType={squares[2]} onSquareClick={() => handleClick(2)}/>
     </div>
     <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
+      <Square markType={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square markType={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square markType={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
+      <Square markType={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square markType={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square markType={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
     </>
   )
