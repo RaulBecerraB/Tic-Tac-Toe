e2a9e3f5a7e7469b06c8e0222a4bd16b01e3cfa4
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2024-08-08 00:03:52 -0600

    feat: add calculateWinner function

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const Board = () =>
   const [xIsNext, setIsNext] = useState(true)
 
   const handleClick = (i) => {
-    
+
     //Checks if the clicked square has already something inside it
     if (squares[i]){
       return
@@ -78,4 +78,25 @@ const Square = ({markType, onSquareClick}) =>
       </button>
     )
   }
+
+const calculateWinner = () => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default Board
